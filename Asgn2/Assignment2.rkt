29bb6fa2f1f131603a894fc014d59ba01c716d00
ConnorBoulais <connorboulais@gmail.com>
2020-10-01 14:15:53 -0700

    Add Function Applications

--- Asgn2/Assignment2.rkt
+++ Asgn2/Assignment2.rkt
@@ -8,11 +8,12 @@
 ; Data Definitions:
 ;   ExprC, FunDefC
 
-(define-type ExprC (U NumC PlusC MultC IdC))
+(define-type ExprC (U NumC PlusC MultC IdC AppC))
 (struct NumC ([n : Real]) #:transparent)
 (struct PlusC ([l : ExprC] [r : ExprC]) #:transparent)
 (struct MultC ([l : ExprC] [r : ExprC]) #:transparent)
 (struct IdC ([s : Symbol]) #:transparent)
+(struct AppC ([fun : Symbol] [arg : ExprC]) #:transparent)
 
 (struct FunDefC ([name : Symbol] [arg : Symbol] [body : ExprC]) #:transparent)
 
@@ -28,6 +29,7 @@
     [(list '* a b) (MultC (parse a) (parse b))]
     [(list '- a b) (PlusC (parse a) (MultC (NumC -1)(parse b)))]
     [(list '- a) (MultC (NumC -1) (parse a))]
+    [(list (? symbol? n) (? list? a)) (AppC n (parse a))]
     [else (error 'parse "invalid input to parse")]))
 
 (check-equal? (parse '(+ 1 2))
@@ -38,6 +40,8 @@
               (MultC (MultC (NumC -1) (NumC 1)) (NumC 2)))
 (check-equal? (parse '(- 1 (+ 5 6)))
               (PlusC (NumC 1) (MultC (NumC -1) (PlusC (NumC 5) (NumC 6)))))
+(check-equal? (parse '(func (+ 1 5)))
+              (AppC 'func (PlusC (NumC 1) (NumC 5))))
 (check-exn (regexp (regexp-quote "invalid input to parse"))
            (lambda () (parse "hello")))
 
@@ -54,18 +58,63 @@
            (lambda () (parse-fundef '(+ 1 4))))
 
 ;-------------------------------------------------------------------------------------------
+; substitute
+
+;;substitutes an expression for a symbol in another expression
+(define (subst [what : ExprC] [for : Symbol] [in : ExprC]) : ExprC
+  (match in
+    [(NumC n) in]
+    [(IdC s) (cond
+               [(symbol=? s for) what]
+               [else in])]
+    [(AppC f a) (AppC f (subst what for a))]
+    [(PlusC l r) (PlusC (subst what for l)
+                        (subst what for r))]
+    [(MultC l r) (MultC (subst what for l)
+                        (subst what for r))]))
+
+(check-equal? (subst (NumC 4) 'x (PlusC (IdC 'x) (NumC 1)))
+              (PlusC (NumC 4) (NumC 1)))
+(check-equal? (subst (NumC 4) 'x (AppC 'func (MultC (IdC 'x) (IdC 'y))))
+              (AppC 'func (MultC (NumC 4) (IdC 'y))))
+
+;-------------------------------------------------------------------------------------------
+; get-fundef
+
+;;gets a function definition from a list of FunDefC's, given its name
+(define (get-fundef [n : Symbol] [fds : (Listof FunDefC)]) : FunDefC
+    (cond
+      [(empty? fds)
+       (error 'get-fundef "reference to undefined function")]
+      [(cons? fds)
+       (cond
+         [(equal? n (FunDefC-name (first fds))) (first fds)]
+         [else (get-fundef n (rest fds))])]))
+
+(check-equal? (get-fundef 'func1 (list (FunDefC 'func2 'x (NumC 4))
+                                       (FunDefC 'func1 'x (NumC 5))))
+              (FunDefC 'func1 'x (NumC 5)))
+(check-exn (regexp (regexp-quote "get-fundef: reference to undefined function"))
+           (lambda () (get-fundef 'func '())))
+
+;-------------------------------------------------------------------------------------------
 ; interp
 
 ;;evaluates an ExprC to a value
-(define (interp [a : ExprC]) : Real
+(define (interp [a : ExprC] [fds : (Listof FunDefC)]) : Real
     (match a
       [(NumC n) n]
-      [(PlusC l r) (+ (interp l) (interp r))]
-      [(MultC  l r) (* (interp l) (interp r))]))
+      [(PlusC l r) (+ (interp l fds) (interp r fds))]
+      [(MultC  l r) (* (interp l fds) (interp r fds))]
+      [(AppC f a) (define fd (get-fundef f fds))
+                  (interp (subst a (FunDefC-arg fd) (FunDefC-body fd)) fds)]))
 
-(check-= (interp (PlusC (NumC 4) (NumC 5))) 9 EPSILON)
-(check-= (interp (MultC (NumC 4) (NumC 5))) 20 EPSILON)
-(check-= (interp (PlusC (MultC (NumC 3) (NumC 2)) (NumC 5))) 11 EPSILON)
+(check-= (interp (PlusC (NumC 4) (NumC 5)) '()) 9 EPSILON)
+(check-= (interp (MultC (NumC 4) (NumC 5)) '()) 20 EPSILON)
+(check-= (interp (PlusC (MultC (NumC 3) (NumC 2)) (NumC 5)) '()) 11 EPSILON)
+(check-= (interp (AppC 'func (NumC 3))
+                 (list (FunDefC 'func 'x (PlusC (IdC 'x) (NumC 1)))))
+          4 EPSILON)
 
 
 ;-------------------------------------------------------------------------------------------
@@ -73,7 +122,7 @@
 
 ;;interperates an S-expression
 (define (top-interp [s : Sexp]) : Real
-  (interp (parse s)))
+  (interp (parse s) '()))
 
 (check-equal? (top-interp '{+ 2 3}) 5)
 (check-equal? (top-interp '{* 2 {- {- {- 5 2}}}}) 6)
