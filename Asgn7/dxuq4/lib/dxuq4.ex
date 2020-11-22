12f809c2048c39fb1ea119edda7de85cbfa938ac
ConnorBoulais <connorboulais@gmail.com>
2020-11-22 12:10:13 -0800

    init Asgn7

---
+++ Asgn7/dxuq4/lib/dxuq4.ex
@@ -0,0 +1,16 @@
+defmodule Dxuq4 do
+  @moduledoc """
+  Documentation for `Dxuq4`.
+  """
+
+  @doc """
+  Hello world.
+  """
+  def hello do
+    :world
+  end
+end
+
+defmodule NumC do
+  defstruct val: nil
+end
